f3e371e152b790daa7b280c7928e9b863e08e133
Ege Özel <egeeozel@gmail.com>
2026-08-20 18:49:55 +0300

    feat: wire orbit/pan/zoom mouse input to camera

--- woodcraft/app.py
+++ woodcraft/app.py
@@ -1,8 +1,9 @@
 # woodcraft/app.py
 from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
 from panda3d.core import AmbientLight, DirectionalLight, LineSegs
 
-from woodcraft.camera_math import orbit_position
+from woodcraft.camera_math import orbit_position, pan_target, update_orbit_angles, zoom_distance
 from woodcraft.piece import Piece, build_piece_nodepath
 
 GRID_INCREMENT = 1.0  # inches
@@ -59,6 +60,21 @@ class WoodcraftApp(ShowBase):
         self.cam_distance = 120.0
         self.update_camera_pos()
 
+        self._last_mouse = None
+        self._orbiting = False
+        self._panning = False
+        self._dragging_piece = None  # populated by Task 9/10
+        self._selected = None  # populated by Task 9
+
+        self.accept("mouse1", self._on_mouse1_down)
+        self.accept("mouse1-up", self._on_mouse1_up)
+        self.accept("mouse3", self._on_mouse3_down)
+        self.accept("mouse3-up", self._on_mouse3_up)
+        self.accept("wheel_up", self._on_wheel_up)
+        self.accept("wheel_down", self._on_wheel_down)
+
+        self.task_mgr.add(self._on_frame, "woodcraft-update")
+
     def _setup_lighting(self):
         ambient = AmbientLight("ambient")
         ambient.set_color((0.35, 0.35, 0.35, 1.0))
@@ -76,3 +92,61 @@ class WoodcraftApp(ShowBase):
         pos = orbit_position(self.cam_target, self.cam_heading, self.cam_pitch, self.cam_distance)
         self.camera.set_pos(*pos)
         self.camera.look_at(*self.cam_target)
+
+    def _on_mouse1_down(self):
+        self._orbiting = True
+
+    def _on_mouse1_up(self):
+        self._orbiting = False
+        self._dragging_piece = None
+
+    def _on_mouse3_down(self):
+        self._panning = True
+
+    def _on_mouse3_up(self):
+        self._panning = False
+
+    def _on_wheel_up(self):
+        self.cam_distance = zoom_distance(self.cam_distance, wheel_delta=1)
+        self.update_camera_pos()
+
+    def _on_wheel_down(self):
+        self.cam_distance = zoom_distance(self.cam_distance, wheel_delta=-1)
+        self.update_camera_pos()
+
+    def _camera_right_up(self):
+        quat = self.camera.get_quat(self.render)
+        return tuple(quat.get_right()), tuple(quat.get_up())
+
+    def _on_frame(self, task):
+        if not self.mouseWatcherNode.has_mouse():
+            self._last_mouse = None
+            return Task.cont
+
+        mouse = self.mouseWatcherNode.get_mouse()
+        current = (mouse.get_x(), mouse.get_y())
+
+        if self._last_mouse is None:
+            self._last_mouse = current
+            return Task.cont
+
+        dx = current[0] - self._last_mouse[0]
+        dy = current[1] - self._last_mouse[1]
+
+        if self._dragging_piece is not None:
+            self._update_drag(mouse)
+        elif self._orbiting:
+            self.cam_heading, self.cam_pitch = update_orbit_angles(
+                self.cam_heading, self.cam_pitch, dx, dy
+            )
+            self.update_camera_pos()
+        elif self._panning:
+            right, up = self._camera_right_up()
+            self.cam_target = pan_target(self.cam_target, right, up, dx, dy)
+            self.update_camera_pos()
+
+        self._last_mouse = current
+        return Task.cont
+
+    def _update_drag(self, mouse):
+        pass  # implemented in Task 10
